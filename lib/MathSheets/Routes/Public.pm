2115f5ba5ee02257cd3768280cf9d43ed6bed6ff
Naveed Massjouni <nmassjouni@harmonia.com>
2020-08-22 06:57:33 +0000

    Convert home page to React

--- lib/MathSheets/Routes/Public.pm
+++ lib/MathSheets/Routes/Public.pm
@@ -1,12 +1,9 @@
 package MathSheets::Routes::Public;
 use Dancer ':syntax';
 
-use Dancer::Plugin::DBIC qw(schema);
-use Dancer::Plugin::Res;
-
 get '/' => sub {
     return redirect uri_for '/teacher/students' if session 'teacher';
-    return template 'welcome';
+    return template 'home';
 };
 
 get '/help' => sub { template 'help' };
