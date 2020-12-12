9a1192755ea79e9bb74bfb96804e4965f7383d17
Naveed Massjouni <nmassjouni@harmonia.com>
2020-12-12 03:36:19 +0000

    Remove old files

--- config.example.js
+++ config.example.js
@@ -4,13 +4,23 @@ module.exports = {
   admin_email: 'admin@mathbombs.org',
   admin_password: '...',
 
+  base_url: 'https://mathbombs.org',
+
   email: {
-    host: 'smtp.mailgun.org',
-    port: 587,
-    auth: {
-      user: '...',
-      pass: '...',
+    from: 'notifications@mathbombs.org',
+    nodemailer: {
+      host: 'smtp.mailgun.org',
+      port: 587,
+      auth: {
+        user: '...',
+        pass: '...',
+      },
     },
+  },
+
+  powerups: {
+    1: { probability: 10 },
+    2: { probability: 2  },
   },
 
 };
--- data/dbicdump.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-dbicdump -o dump_directory=/opt/MathSheets/lib \
- -o relationship_attrs='{
-    has_many => {cascade_copy => 1, cascade_delete => 1},
-}' MathSheets::Schema dbi:SQLite:/opt/MathSheets/data/math.db
-#belongs_to => {on_delete => "CASCADE", on_update => "CASCADE"},
-#-o components='[qw(InflateColumn::DateTime)]' \
